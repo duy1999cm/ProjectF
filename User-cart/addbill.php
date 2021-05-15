401c1f45f5caae108d4e84cc7220f189b3c7c751
Stealth <duy1999cm@gmail.com>
2021-05-15 21:03:53 +0700

    bill

--- User-cart/addbill.php
+++ User-cart/addbill.php
@@ -3,6 +3,7 @@ require_once '../config/dbhelper.php';
 $id=random_int(100000,999999);
 $userid=$_SESSION['user_id'];
 $username=$_SESSION['name'];
+$phone=$_SESSION['phone'];
 $address=$_GET['address'];
 $payment=$_GET['payment'];
 $sum=$_GET['sum'];
@@ -12,7 +13,7 @@ if($address==""){
         Bạn chưa cập nhật địa chỉ!
     ';
 }else{
-    $sql="INSERT INTO `bill`(`id`,`username`, `address`, `date`, `payment`, `sum`) VALUES ('$id','$username','$address','$today','$payment','$sum')";
+    $sql="INSERT INTO `bill`(`id`,`username`, `address`,`phone`,`date`, `payment`, `sum`) VALUES ('$id','$username','$address','$phone','$today','$payment','$sum')";
     execute($sql);
     $sql1 = "SELECT * FROM cart where user_id='$userid'";
     $reusult = executeResult($sql1);
